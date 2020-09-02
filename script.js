29773d2bf10fb503c52e818fdaf31754c176e053
Emmanuel Durand <shernua@gmail.com>
2020-09-02 15:51:26 -0500

    modification for appearance on screen

--- script.js
+++ script.js
@@ -3,36 +3,40 @@
 
             var cityName = $(".form-control").val();
 
-
             // Storing our giphy API URL for a random cat image
-            var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=837d2d3fb19dcc5f4d0f563d1c4af81f";
+            var queryURLCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=837d2d3fb19dcc5f4d0f563d1c4af81f";
+            var queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=837d2d3fb19dcc5f4d0f563d1c4af81f";
 
             // Perfoming an AJAX GET request to our queryURL
             $.ajax({
-                url: queryURL,
+                url: queryURLCurrent,
                 method: "GET"
             })
 
             // After the data from the AJAX request comes back
             .then(function(response) {
+                $("#cityName").text(response.name + " (" + moment().format('L') + ")");
+                $("#temperature").text((response.main.temp - 273.15).toFixed(2));
+                $("#humidity").text(response.main.humidity);
+                $("#wind").text((response.wind.speed / 1.609344).toFixed(2));
 
-                console.log(response.name);
-                console.log(moment().format('L'));
-                console.log((response.main.temp - 273.15).toFixed(2));
-                console.log(response.main.humidity);
-                console.log(response.wind.speed);
+            })
 
-                // // Saving the image_original_url property
-                // var imageUrl = response.data.image_original_url;
 
-                // // Creating and storing an image tag
-                // var catImage = $("<img>");
 
-                // // Setting the catImage src attribute to imageUrl
-                // catImage.attr("src", imageUrl);
-                // catImage.attr("alt", "cat image");
+            // Perfoming an AJAX GET request to our queryURL
 
-                // // Prepending the catImage to the images div
-                // $("#images").prepend(catImage);
+            $.ajax({
+                url: queryURLForecast,
+                method: "GET"
+            })
+
+            // After the data from the AJAX request comes back
+            .then(response => {
+
+                console.log(moment().add(1, 'days').calendar());
+                console.log((response.list[1].main.temp - 273.15).toFixed(2));
+                console.log(response.list[1].main.humidity);
+
             });
         });
