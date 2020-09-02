2eb8a0c9a40b8342aedc04b1ded0a5b1e0fc5538
Emmanuel Durand <shernua@gmail.com>
2020-09-02 14:41:15 -0500

    work on js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
         // Event listener for our cat-button
-        $("#cat-button").on("click", function() {
+        $(".btn").on("click", function() {
+
+            var cityName = $(".form-control").val();
+
 
             // Storing our giphy API URL for a random cat image
-            var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + boston + "&appid=837d2d3fb19dcc5f4d0f563d1c4af81f";
+            var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=837d2d3fb19dcc5f4d0f563d1c4af81f";
 
             // Perfoming an AJAX GET request to our queryURL
             $.ajax({
@@ -13,17 +16,23 @@
             // After the data from the AJAX request comes back
             .then(function(response) {
 
-                // Saving the image_original_url property
-                var imageUrl = response.data.image_original_url;
+                console.log(response.name);
+                console.log(moment().format('L'));
+                console.log((response.main.temp - 273.15).toFixed(2));
+                console.log(response.main.humidity);
+                console.log(response.wind.speed);
 
-                // Creating and storing an image tag
-                var catImage = $("<img>");
+                // // Saving the image_original_url property
+                // var imageUrl = response.data.image_original_url;
 
-                // Setting the catImage src attribute to imageUrl
-                catImage.attr("src", imageUrl);
-                catImage.attr("alt", "cat image");
+                // // Creating and storing an image tag
+                // var catImage = $("<img>");
 
-                // Prepending the catImage to the images div
-                $("#images").prepend(catImage);
+                // // Setting the catImage src attribute to imageUrl
+                // catImage.attr("src", imageUrl);
+                // catImage.attr("alt", "cat image");
+
+                // // Prepending the catImage to the images div
+                // $("#images").prepend(catImage);
             });
         });
